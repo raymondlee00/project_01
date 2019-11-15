96c3f7e7464ed3bd90b184d6a4c64795f8d79b74
EvilJesster <jhall00@stuy.edu>
2019-11-15 10:53:13 -0500

    messing with opensky python api, working geolocation,

--- apitesting.py
+++ apitesting.py
@@ -1,6 +1,38 @@
-import json, urllib3
+import json, urllib3, requests
+from opensky_api import OpenSkyApi
 
+
+
+#opensky stuff goes here
 http = urllib3.PoolManager()
-r = http.request('GET', 'https://opensky-network.org/api/states/all?lamin=45.8389&lomin=5.9962&lamax=47.8229&lomax=10.5226')
-r.data
-print(r.data)
+#r = http.request('GET', 'https://opensky-network.org/api/states/all?lamin=45.8389&lomin=5.9962&lamax=47.8229&lomax=10.5226')
+# r.data
+# print(r.data)
+
+
+
+
+
+def planeswithin(minlat, maxlat, minlong, maxlong):
+    api = OpenSkyApi()
+    states = api.get_states(bbox=(minlat, maxlat, minlong, maxlong))
+    for s in states.states:
+        print("(%r, %r, %r, %r)" % (s.longitude, s.latitude, s.baro_altitude, s.velocity))
+
+
+
+
+
+
+#geolocation stuff goes here
+
+
+
+def location():
+    ip_grabber = requests.get('https://get.geojs.io/v1/ip.json')
+    my_ip = ip_grabber.json()['ip']
+    reqbuilder = 'https://get.geojs.io/v1/ip/geo/' + my_ip + '.json'
+    loc_req = requests.get(reqbuilder)
+    loc_data = loc_req.json()
+    return(loc_data)
+
