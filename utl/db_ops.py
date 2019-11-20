321b1cc6091ebf074ffabc6d144a12afa7128f2a
ray. lee <rlee00@stuy.edu>
2019-11-19 20:11:12 -0500

    some login stuff

---
+++ utl/db_ops.py
@@ -0,0 +1,64 @@
+import sqlite3
+
+DB_FILE = "discobandit.db"
+
+def accountExists(user):
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()               #facilitate db ops
+
+    #==========================================================
+
+    c.execute(
+    """
+        SELECT * FROM accounts WHERE username = (?)
+    """, (user,)
+    )
+
+    rowCount = 0
+    for row in c:
+        rowCount += 1
+
+    #==========================================================
+
+    db.close()  #close database
+
+    if (rowCount == 1):
+        return True
+
+    return False
+
+# function to takes in a username and password and creates a new user entry
+def addAccount(user, pw):
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()               #facilitate db ops
+
+    #==========================================================
+
+    c.execute("INSERT INTO accounts VALUES (?, ?)", (user, pw))
+
+    #==========================================================
+
+    db.commit() #save changes
+    db.close()  #close database
+
+# function that authenticates the username and password arguments to verify that the account exists
+def authenticate(user, pw):
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()               #facilitate db ops
+
+    #==========================================================
+
+    c.execute(
+    """
+        SELECT * FROM accounts WHERE username = (?)
+    """, (user,)
+    )
+
+    rowCount = 0
+    for row in c:
+        db.close() #only one iteration should happen anyway so I can close it right now
+        rowCount += 1
+        if (rowCount != 1):
+            return False
+
+        return pw == row[1]
