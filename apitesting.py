78e5a0fed61c1a0c0ae2c82c9006dfa2f8325cbf
EvilJesster <jhall00@stuy.edu>
2019-12-03 08:38:48 -0500

    I am not explaining this ray

--- apitesting.py
+++ apitesting.py
@@ -11,17 +11,21 @@ def planeswithin(minlat, maxlat, minlong, maxlong, test = False):
         holddata = restreq.text
         holddata = holddata[int(holddata.find("states")) + 8:]
         holddata = holddata.split("],")
-        # print(holddata)        
-        holdout = []
-        for idx,i in enumerate(holddata):
-            holddata[idx] = i.split(',')
-            if idx == 0:
-                holddata[idx][0] = holddata[idx][0][holddata[idx][0].find("[[") + 2:]
-            if idx == (len(holddata) - 1):
-                print("holddata idx: {}".format(holddata[idx]))
-                print("holddata arr: {}".format(holddata[idx][16][0]))
-                holddata[idx][16] = holddata[idx][16][0]
+        # print(holddata)
+        try:
+            for idx,i in enumerate(holddata):
+                holddata[idx] = i.split(',')
+                if idx == 0:
+                    holddata[idx][0] = holddata[idx][0][holddata[idx][0].find("[[") + 2:]
+                if idx == (len(holddata) - 1):
+                    print("holddata idx: {}".format(holddata[idx]))
+             #       print("holddata arr: {}".format(holddata[idx][16][0]))
+                    holddata[idx][16] = holddata[idx][16][0]
+        except:
+            print('hell')
     # print(holddata[0])
+  #  if(len(holddata) == 0):
+
     return (holddata)
 
 
